6ca88b2ac6c06c2af8c5f9093d9174ca77277352
Lee-Sock <87433030+Lee-Sock@users.noreply.github.com>
2023-11-22 15:15:19 +0800

    update

--- webStore/exampleCode/test.php
+++ webStore/exampleCode/test.php
@@ -5,14 +5,15 @@ $con = mysqli_connect("localhost","root","","phase2"); //connect to database
 if (!$con){
     die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail 
 } 
-$query= $con->prepare("INSERT INTO `user` (`email`, `password`, `username`, `firstname`,`lastname` , `phoneno`) VALUES (?,?,?,?,?,?,?)");  
+$query= $con->prepare("INSERT INTO `user` (`userid`, `email`, `password`, `username`, `firstname`,`lastname` , `phoneno`) VALUES (?,?,?,?,?,?,?)");  
+$userid = '1';
 $email = 'example@examaple.com'; 
 $password = '123'; 
 $username = 'test user'; 
 $firstname = 'way'; 
 $lastname = 'hng'; 
 $phoneno='12345678';
-$query->bind_param($email,$password, $username, $firstname, $lastname, $phoneno); //bind the parameters 
+$query->bind_param($userid,$email,$password, $username, $firstname, $lastname, $phoneno); //bind the parameters 
 if ($query->execute()){ //execute query 
     echo "Query executed."; 
 }else{ 
