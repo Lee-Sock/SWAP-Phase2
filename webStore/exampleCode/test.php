3d653214d1c05aa1ea34331342dc017ebe4a8fc5
Lee-Sock <87433030+Lee-Sock@users.noreply.github.com>
2023-11-22 15:34:13 +0800

    Update test.php

--- webStore/exampleCode/test.php
+++ webStore/exampleCode/test.php
@@ -13,12 +13,12 @@ $username = 'test user';
 $firstname = 'way'; 
 $lastname = 'hng'; 
 $phoneno=12345678;
-$query->bind_param('sssd',$userid, $email,$password, $username, $firstname, $lastname, $phoneno); //bind the parameters 
-if ($query->execute()){ //execute query 
-    echo "Query executed."; 
-}else{ 
-    echo "Error executing query."; 
-} 
-?> 
+$query->bind_param('sssssss',$userid, $email,$password, $username, $firstname, $lastname, $phoneno); //bind the parameters 
+if ($query->execute()){ //execute query
+    echo "Query executed.";
+}else{
+    echo "Error executing query.";
+}
+?>
 </body> 
 </html>
