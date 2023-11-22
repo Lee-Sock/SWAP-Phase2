5942e9a9cf0af2260b913704d6fae82f65dad7f6
Lee-Sock <87433030+Lee-Sock@users.noreply.github.com>
2023-11-22 16:36:06 +0800

    crud example code complete

--- webStore/exampleCode/delete.php
+++ webStore/exampleCode/delete.php
@@ -1,1 +1,20 @@
+<html>
+<body>
 <?php
+$con = mysqli_connect("localhost","root","","phase2"); //connect to database
+if (!$con){
+    die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
+}
+$query= $con->prepare("DELETE FROM user WHERE userid=?");
+
+$userid = 1;
+
+$query->bind_param('s', $userid); //bind the parameters
+if ($query->execute()){
+    echo "Query executed.";
+}else{
+    echo "Error executing query."; 
+}
+?>
+</body>
+</html>
--- webStore/exampleCode/read.php
+++ webStore/exampleCode/read.php
@@ -1,1 +1,42 @@
+<html>
+<body>
 <?php
+$servername = "localhost";
+$dbname ="phase2";
+
+$con = new PDO("mysql:host=$servername;dbname=$dbname","root",""); //connect to database
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query= $con->prepare("SELECT * FROM user");
+
+$query->execute();
+
+$result = $query->fetchALL(PDO::FETCH_ASSOC);
+
+?>
+
+<table style "margin: 0 auto; text-align: center;" border '1'>
+	<tr>
+		<th>userid</th>
+		<th>email</th>
+		<th>password</th>
+		<th>username</th>
+		<th>firstname</th>
+		<th>lastname</th>
+		<th>phoneno</th>
+	<tr>
+	<?php foreach ($result as $row) { ?>
+		<tr>
+			<td><?= isset($row['userid']) ? $row['userid'] : '' ?></td>
+			<td><?= isset($row['email']) ? $row['email'] : '' ?></td>
+			<td><?= isset($row['password']) ? $row['password'] : '' ?></td>
+			<td><?= isset($row['username']) ? $row['username'] : '' ?></td>
+			<td><?= isset($row['firstname']) ? $row['firstname'] : '' ?></td>
+			<td><?= isset($row['lastname']) ? $row['lastname'] : '' ?></td>
+			<td><?= isset($row['phoneno']) ? $row['phoneno'] : '' ?></td>
+		</tr>
+	<?php } ?>
+</table>
+
+</body>
+</html>
