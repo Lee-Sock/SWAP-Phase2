e18ea6e77e4d141178bc3afc24ea412e562598bf
dharaaaaa528 <155510942+dharaaaaa528@users.noreply.github.com>
2024-01-09 12:51:07 +0800

    Create usermain.php

---
+++ webStore/public/usermain.php
@@ -0,0 +1,18 @@
+<?php
+require_once "index1.php";
+require_once "adminInventory.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+
+</body>
+
+
+</html>
