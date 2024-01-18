a4ee23c214ae2f46a07bf88b37e66a0c5c87b8da
dharaaaaa528 <155510942+dharaaaaa528@users.noreply.github.com>
2024-01-18 23:23:30 +0800

    Update registration.php

--- webStore/public/registration.php
+++ webStore/public/registration.php
@@ -1,11 +1,62 @@
 <?php 
+// require_once 'config.php';
+
+// if(isset($_SESSION["login"]) && $_SESSION["login"] === true){
+//     header("Location: addtocart.php");
+//     exit();
+// }
+// if(isset($_POST["submit"])){
+//     $firstname = $_POST["firstname"];
+//     $lastname = $_POST["lastname"];
+//     $username = $_POST["username"];
+//     $email = $_POST["email"];
+//     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+//     $phoneno = $_POST["phoneno"];
+
+//     $duplicate = mysqli_query($conn, "SELECT * FROM user
+//                                       WHERE username = '$username'
+//                                       OR email = '$email'
+//                                       OR phoneno = '$phoneno'");
+    
+//     if (mysqli_num_rows($duplicate) > 0) {
+//         echo
+//         "<script> alert('Username or Email or PhoneNo Is Already Taken'); </script>";
+        
+//     }
+//     else{
+//         $query = "INSERT INTO user VALUES('', '$email','$password','$username','$firstname','$lastname','$phoneno')";
+//         mysqli_query($conn,$query);
+//         $query2 = $con->prepare("SELECT * FROM user WHERE username = ?");
+//         $query2 ->bindValue(1,$username); //bind the parameters
+//         $query2 -> execute();
+//         $resultUserid = $query2->fetchALL();
+//         foreach ($resultUserid as $row){
+//             $query3= $conn->prepare("INSERT INTO `cart` (`cartid`, `userid`) VALUES (?,?)");
+//             $query3->bind_param('ss',$row["userid"], $row["userid"]); //bind the parameters
+//             if ($query3->execute()){ //execute query
+//                 echo "";
+//             }else{
+//                 echo "Error executing query.";
+//             }
+//         }
+//         echo
+//         "<script> alert('Registration Successful'); </script>";
+        
+//     }
+    
+   
+// }
+// ?>
+
+<?php
 require_once 'config.php';
 
-if(isset($_SESSION["login"]) && $_SESSION["login"] === true){
+if (isset($_SESSION["login"]) && $_SESSION["login"] === true) {
     header("Location: addtocart.php");
     exit();
 }
-if(isset($_POST["submit"])){
+
+if (isset($_POST["submit"])) {
     $firstname = $_POST["firstname"];
     $lastname = $_POST["lastname"];
     $username = $_POST["username"];
@@ -13,39 +64,53 @@ if(isset($_POST["submit"])){
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $phoneno = $_POST["phoneno"];
 
-    $duplicate = mysqli_query($conn, "SELECT * FROM user
-                                      WHERE username = '$username'
-                                      OR email = '$email'
-                                      OR phoneno = '$phoneno'");
-    
-    if (mysqli_num_rows($duplicate) > 0) {
-        echo 
-        "<script> alert('Username or Email or PhoneNo Is Already Taken'); </script>";
-        
-    }
-    else{
-        $query = "INSERT INTO user VALUES('', '$email','$password','$username','$firstname','$lastname','$phoneno')";
-        mysqli_query($conn,$query);
-        $query2 = $con->prepare("SELECT * FROM user WHERE username = ?");
-        $query2 ->bindValue(1,$username); //bind the parameters
-        $query2 -> execute();
-        $resultUserid = $query2->fetchALL();
-        foreach ($resultUserid as $row){
-            $query3= $conn->prepare("INSERT INTO `cart` (`cartid`, `userid`) VALUES (?,?)");
-            $query3->bind_param('ss',$row["userid"], $row["userid"]); //bind the parameters
-            if ($query3->execute()){ //execute query
-                echo "";
-            }else{
-                echo "Error executing query.";
+    $duplicate = mysqli_prepare($conn, "SELECT * FROM user
+                                      WHERE username = ? OR email = ? OR phoneno = ?");
+    mysqli_stmt_bind_param($duplicate, "sss", $username, $email, $phoneno);
+    mysqli_stmt_execute($duplicate);
+    mysqli_stmt_store_result($duplicate);
+
+    if (mysqli_stmt_num_rows($duplicate) > 0) {
+        echo "<script> alert('Username or Email or PhoneNo Is Already Taken'); </script>";
+    } else {
+        $insertQuery = mysqli_prepare($conn, "INSERT INTO user (email, password, username, firstname, lastname, phoneno) VALUES (?, ?, ?, ?, ?, ?)");
+        if ($insertQuery) {
+            mysqli_stmt_bind_param($insertQuery, "ssssss", $email, $password, $username, $firstname, $lastname, $phoneno);
+            
+            if (mysqli_stmt_execute($insertQuery)) {
+                $userid = mysqli_insert_id($conn);
+
+                $query3 = mysqli_prepare($conn, "INSERT INTO `cart` (`cartid`, `userid`) VALUES (NULL, ?)");
+                mysqli_stmt_bind_param($query3, 'i', $userid);
+
+                if (mysqli_stmt_execute($query3)) {
+                    echo "<script> alert('Registration Successful'); </script>";
+                } else {
+                    echo "Error executing query3: " . mysqli_error($conn);
+                }
+
+                mysqli_stmt_close($query3);
+            } else {
+                echo "Error executing insertQuery: " . mysqli_error($conn);
             }
+
+            mysqli_stmt_close($insertQuery);
+        } else {
+            echo "Error preparing insertQuery: " . mysqli_error($conn);
         }
-        echo 
-        "<script> alert('Registration Successful'); </script>";
-        
     }
-    
+
+    mysqli_close($conn);
 }
 ?>
+
+<!-- The rest of your HTML code remains unchanged. -->
+
+
+
+
+
+
 
 
 
@@ -54,7 +119,7 @@ if(isset($_POST["submit"])){
 	<head>
 		<meta charset="utf-8">
 		 <link rel="stylesheet" href="styles.css">
-		<title>Registration</title>		
+		<title>Registration</title>
 	</head>
 
 	<body style="font-family: Arial, sans-serif;
